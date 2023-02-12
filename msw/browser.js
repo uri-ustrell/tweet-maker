4838898283f71761a5a0581fb0cffc20a625c9cd
Oriol Ustrell <oriol.ustrell@mango.com>
2023-02-12 19:18:06 +0100

    fix: handlers import

--- msw/browser.js
+++ msw/browser.js
@@ -1,6 +1,6 @@
 import { setupWorker } from 'msw';
 
-import { handlers } from './handlers';
+import handlers from './handlers';
 
 // This configures a Service Worker with the given request handlers.
 const worker = setupWorker(...handlers);
